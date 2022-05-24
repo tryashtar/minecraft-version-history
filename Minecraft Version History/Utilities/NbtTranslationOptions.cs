1387ddf6237369f30bcb71d5db08051d941959b3
tryashtar <tryashtar@gmail.com>
2022-05-23 22:02:13 -0600

    Structure file transforms

--- Minecraft Version History/Utilities/NbtTranslationOptions.cs
+++ Minecraft Version History/Utilities/NbtTranslationOptions.cs
@@ -5,29 +5,21 @@ public class NbtTranslationOptions
     public readonly string Extension;
     public readonly string NewExtension;
     public readonly Endianness Endianness;
-    public readonly bool Minified;
+    public readonly SnbtOptions Options;
+    public readonly bool ConvertStructure;
     private readonly List<string> RemoveKeys;
+    private readonly bool Mini;
     public NbtTranslationOptions(YamlMappingNode node)
     {
         Extension = (string)node["extension"];
         if (!Extension.StartsWith('.'))
             Extension = "." + Extension;
         RemoveKeys = node.Go("remove keys").ToStringList() ?? new List<string>();
-        var endian_node = node.TryGet("endian");
-        if (endian_node == null)
-            Endianness = Endianness.Big;
-        else
-            Endianness = ParseEndianness((string)endian_node);
-        var new_ext_node = node.TryGet("new extension");
-        if (new_ext_node == null)
-            NewExtension = ".snbt";
-        else
-            NewExtension = (string)new_ext_node;
-        var mini_node = node.TryGet("minified");
-        if (mini_node == null)
-            Minified = false;
-        else
-            Minified = Boolean.Parse((string)mini_node);
+        Endianness = node.Go("endian").ToEnum<Endianness>() ?? Endianness.Big;
+        NewExtension = node.TryGet("new extension").String() ?? ".snbt";
+        Mini = node.Go("minified").NullableStructParse(x => Boolean.Parse(x.String())) ?? false;
+        Options = Mini ? SnbtOptions.Default : SnbtOptions.DefaultExpanded;
+        ConvertStructure = node.Go("structure").NullableStructParse(x => Boolean.Parse(x.String())) ?? false;
     }
 
     public bool ShouldTranslate(string path)
@@ -41,25 +33,130 @@ public class NbtTranslationOptions
         if (Endianness == Endianness.Little)
             file.BigEndian = false;
         file.LoadFromFile(path);
+        var root = (NbtCompound)file.RootTag;
         if (RemoveKeys.Any())
         {
             foreach (var key in RemoveKeys)
             {
-                ((NbtCompound)file.RootTag).Remove(key);
+                root.Remove(key);
             }
             file.SaveToFile(path, file.FileCompression);
         }
-        SnbtOptions options = Minified ? SnbtOptions.Default : SnbtOptions.DefaultExpanded;
-        File.WriteAllText(Path.ChangeExtension(path, NewExtension), file.RootTag.ToSnbt(options) + "\n");
+        if (ConvertStructure)
+        {
+            var palettes = root.Get<NbtList>("palettes");
+            var palette = palettes != null ? (NbtList)palettes[0] : root.Get<NbtList>("palette");
+            var new_palette = new NbtList(palette.Tags.Cast<NbtCompound>().Select(PackBlockState));
+            root["palette"] = new_palette;
+            if (palettes != null)
+            {
+                var new_palettes = new NbtList();
+                foreach (NbtList p in palettes)
+                {
+                    var compound = new NbtCompound();
+                    for (int i = 0; i < palettes.Count; i++)
+                    {
+                        compound[i.ToString()] = PackBlockState((NbtCompound)p[i]);
+                    }
+                }
+                root["palettes"] = new_palettes;
+            }
+            var entities = root.Get<NbtList>("entities");
+            if (entities != null)
+            {
+                var sorted = entities.Tags.Cast<NbtCompound>()
+                    .OrderBy(x => x.Get<NbtList>("pos")[1].DoubleValue)
+                    .ThenBy(x => x.Get<NbtList>("pos")[0].DoubleValue)
+                    .ThenBy(x => x.Get<NbtList>("pos")[2].DoubleValue).ToList();
+                entities.Clear();
+                root["entities"] = new NbtList(sorted);
+            }
+            var blocks = root.Get<NbtList>("blocks");
+            var new_blocks = blocks.Tags.Cast<NbtCompound>()
+                .OrderBy(x => x.Get<NbtList>("pos")[1].IntValue)
+                .ThenBy(x => x.Get<NbtList>("pos")[0].IntValue)
+                .ThenBy(x => x.Get<NbtList>("pos")[2].IntValue).ToList();
+            blocks.Clear();
+            foreach (var block in new_blocks)
+            {
+                var state = block.Get<NbtInt>("state");
+                block["state"] = (NbtTag)new_palette[state.Value].Clone();
+                block.Sort(new BlockOrder(), false);
+            }
+            root["blocks"] = new NbtList(new_blocks);
+            root["blocks"].Name = "data";
+        }
+        root.Sort(new StructureOrder(), false);
+        if (!Mini)
+        {
+            Options.ShouldIndent = tag =>
+            {
+                if (tag.Parent == null || tag.Parent == root)
+                    return true;
+                var top_parent = tag;
+                while (top_parent.Parent != root)
+                {
+                    top_parent = top_parent.Parent;
+                }
+                if (top_parent == root["data"])
+                    return false;
+                if (top_parent == root["entities"])
+                    return false;
+                return true;
+            };
+        }
+        File.WriteAllText(Path.ChangeExtension(path, NewExtension), root.ToSnbt(Options) + Environment.NewLine);
     }
 
-    private static Endianness ParseEndianness(string str)
+    private class StructureOrder : IComparer<NbtTag>
     {
-        if (String.Equals(str, "big", StringComparison.OrdinalIgnoreCase))
-            return Endianness.Big;
-        else if (String.Equals(str, "little", StringComparison.OrdinalIgnoreCase))
-            return Endianness.Little;
-        throw new ArgumentException($"Invalid endianness: {str}");
+        public int Compare(NbtTag x, NbtTag y)
+        {
+            return Index(x.Name).CompareTo(Index(y.Name));
+        }
+        private int Index(string name)
+        {
+            if (name == "size")
+                return 0;
+            if (name == "data")
+                return 1;
+            if (name == "entities")
+                return 2;
+            if (name == "palette")
+                return 3;
+            return 4;
+        }
+    }
+    private class BlockOrder : IComparer<NbtTag>
+    {
+        public int Compare(NbtTag x, NbtTag y)
+        {
+            return Index(x.Name).CompareTo(Index(y.Name));
+        }
+        private int Index(string name)
+        {
+            if (name == "pos")
+                return 0;
+            if (name == "state")
+                return 1;
+            if (name == "nbt")
+                return 2;
+            return 3;
+        }
+    }
+
+    // differs slightly from the Minecraft implementation
+    // emits block states in the form [key=value] instead of {key:value}
+    private NbtString PackBlockState(NbtCompound compound)
+    {
+        var builder = new StringBuilder(compound.Get<NbtString>("Name").Value);
+        var properties = compound.Get<NbtCompound>("Properties");
+        if (properties != null)
+        {
+            string str = String.Join(',', properties.Select(x => x.Name + '=' + x.StringValue));
+            builder.Append('[').Append(str).Append(']');
+        }
+        return new NbtString(builder.ToString());
     }
 }
 
