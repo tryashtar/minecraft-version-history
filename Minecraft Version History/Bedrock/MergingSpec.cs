85b43c11d68e360da0919403b477f8d25fb6e4f8
tryashtar <tryashtar@gmail.com>
2022-10-21 14:59:50 -0600

    Merge lists

--- Minecraft Version History/Bedrock/MergingSpec.cs
+++ Minecraft Version History/Bedrock/MergingSpec.cs
@@ -4,20 +4,20 @@ public class MergingSpec
 {
     private readonly string Extension;
     private readonly string[] Path;
+    private readonly ForwardNodeFinder ListPath;
     private readonly List<string> OverwriteKeys;
     public readonly MergeOperation Operation;
     public readonly KeyMover KeyMover;
     public MergingSpec(YamlMappingNode node)
     {
         var path_node = node.TryGet("path");
-        if (path_node == null)
-            Path = null;
-        else
+        if (path_node != null)
             Path = Util.Split((string)path_node);
+        var list_path_node = node.TryGet("list");
+        if (list_path_node != null)
+            ListPath = new ForwardNodeFinder(list_path_node.ToList(NodeMatcher.Create));
         var ext_node = node.TryGet("extension");
-        if (ext_node == null)
-            Extension = null;
-        else
+        if (ext_node != null)
         {
             Extension = (string)ext_node;
             if (!Extension.StartsWith('.'))
@@ -68,6 +68,25 @@ public class MergingSpec
             }
             File.WriteAllText(current_path, Util.ToMinecraftJson(result));
         }
+        else if (Operation == MergeOperation.AppendList)
+        {
+            var newer = JsonNode.Parse(File.ReadAllText(newer_path), null, new JsonDocumentOptions() { CommentHandling = JsonCommentHandling.Skip });
+            JsonNode result = newer;
+            if (File.Exists(current_path))
+            {
+                result = JsonNode.Parse(File.ReadAllText(current_path));
+                var result_lists = ListPath.FindNodes(result).Select(x => x.node).OfType<JsonArray>().ToList();
+                var newer_lists = ListPath.FindNodes(newer).Select(x => x.node).OfType<JsonArray>().ToList();
+                foreach (var r in result_lists)
+                {
+                    foreach (var n in newer_lists)
+                    {
+                        MergeTopArray(r, n);
+                    }
+                }
+            }
+            File.WriteAllText(current_path, Util.ToMinecraftJson(result));
+        }
         else if (Operation == MergeOperation.AppendLines)
         {
             if (File.Exists(current_path))
@@ -78,6 +97,11 @@ public class MergingSpec
                 input.CopyTo(output);
             }
             else
+                File.Copy(newer_path, current_path);
+        }
+        else if (Operation == MergeOperation.NoMerge)
+        {
+            if (!File.Exists(current_path))
                 File.Copy(newer_path, current_path);
         }
     }
@@ -119,5 +143,7 @@ public class MergingSpec
 public enum MergeOperation
 {
     AppendLines,
-    MergeJson
+    MergeJson,
+    AppendList,
+    NoMerge
 }
